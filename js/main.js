dfc621dffb430b2a9e131cd292a872743b1f945c
miralmag <miriam.almagro@gmail.com>
2022-07-14 17:44:37 +0200

    add conditional to check if input is NaN

--- js/main.js
+++ js/main.js
@@ -16,17 +16,19 @@ function getRandomNumber(max) {
 
 function hintMessage() {
 const numberInputValue = parseInt(numberInput.value);
-    if (numberInputValue === randomNumber) {
+    if (isNaN(numberInputValue))  {
+        hint.innerHTML = `Debes introducir un número`
+    } else if (numberInputValue <= 0) {
+        hint.innerHTML = `El número debe estar entre 1 y 100`
+    } else if (numberInputValue > 100) {
+        hint.innerHTML = `El número debe estar entre 1 y 100`
+    } else if (numberInputValue > randomNumber) {
+        hint.innerHTML = `Pista: Demasiado alto`
+    } else if (numberInputValue < randomNumber) {
+        hint.innerHTML = `Pista: Demasiado bajo`
+    } else if (numberInputValue === randomNumber) {
     hint.innerHTML = `¡Has ganado, campeona!`
-    } if (numberInputValue > randomNumber) {
-    hint.innerHTML = `Pista: Demasiado alto`
-    } if (numberInputValue < randomNumber) {
-    hint.innerHTML = `Pista: Demasiado bajo`
-    } if (numberInputValue <= 0) {
-    hint.innerHTML = `El número debe estar entre 1 y 100`
-    } if (numberInputValue > 100) {
-    hint.innerHTML = `El número debe estar entre 1 y 100`
-}
+    }   
 };
 
 function updateCounter() {
